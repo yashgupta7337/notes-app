d63d81f76fad1b9d20ca8ac5a3e507f755b1963d
Yash Gupta <yashgupta7337@gmail.com>
2025-03-06 01:16:00 +0530

    add home page support for static and templates

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 from typing import Union
-
-from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from fastapi import FastAPI, Request
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
+@app.get("/", response_class=HTMLResponse)
+async def read_item(request: Request):
+    return templates.TemplateResponse(
+        request=request, name="index.html"
+    )
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
